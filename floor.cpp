d8d717768bc05c3acb24d2648e0bb0e2cfc204af
Sankriti Mishra <104777910+sankritimishra@users.noreply.github.com>
2023-06-22 23:11:59 +0530

    Add files via upload

---
+++ floor.cpp
@@ -0,0 +1,104 @@
+/*
+You are given a BST(Binary Search Tree) with n number of nodes and value x. your task is to find the greatest value node of the BST which is smaller than or equal to x.
+Note: when x is smaller than the smallest node of BST then returns -1.
+
+Example:
+
+Input:
+n = 7               2
+                     \
+                      81
+                    /     \
+                 42       87
+                   \       \
+                    66      90
+                   /
+                 45
+x = 87
+Output:
+87
+Explanation:
+87 is present in tree so floor will be 87.
+
+
+*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+int floor(Node *root, int x);
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+
+        cout << floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
+
+
+// Function to search a node in BST.
+
+int floor(Node* root, int x) {
+    // Code here
+    
+    int comp=-1;
+    while(root!=NULL){
+        if(root->data==x){
+            comp=root->data;
+            return comp;
+        }
+        
+        if(x>root->data){
+            comp=root->data;
+            root=root->right;
+        }else{
+            root=root->left;
+        }
+    }
+    
+    return comp;
+}
