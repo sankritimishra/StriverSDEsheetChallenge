72d776045c53250a389fbcf67b14515017f1cb54
Sankriti Mishra <104777910+sankritimishra@users.noreply.github.com>
2023-06-22 23:13:00 +0530

    Add files via upload

---
+++ search.cpp
@@ -0,0 +1,34 @@
+/*
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and 
+return the subtree rooted with that node. If such a node does not exist, return null.
+
+*/
+
+
+  struct TreeNode {
+     int val;
+     TreeNode *left;
+      TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(root==nullptr)return nullptr;
+
+        if(val<root->val){
+            root=searchBST(root->left,val);
+        }else if(val>root->val){
+            root=searchBST(root->right,val);
+        }
+        return root;
+    }
+};
